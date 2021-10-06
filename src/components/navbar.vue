de4075fff3e00e814ba54b8b18ce8f0725ffb0e4
pole <zleckzxd@gmail.com>
2021-10-05 22:52:37 -0300

    base files

---
+++ src/components/navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app-bar app flat>
+    <v-app-bar-title>Rick and Morty</v-app-bar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-icon>mdi-emoticon-cool-outline</v-icon>
+    </v-toolbar-items>
+  </v-app-bar>
+</template>
